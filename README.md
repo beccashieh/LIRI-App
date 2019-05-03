eea4d1ee2a1e3c55ae3c1d572394ab412c2d93d9
BShieh <rebeccasshieh@gmail.com>
2019-05-03 14:05:11 -0400

    updating readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # LIRI-App
+
+LIRI is a language recognition app that operates from the command line. It takes in a command and an optional search parameter to return data utilizing the spotify, bands in town, and OMBD apis. 
+
+## Getting Started
+
+From the command line, type node liri.js followed by one of the following commands:
+* _spotify-this-song_
+* _concert-this_
+* _movie-this_
+* _do-what-it-says_
+
+* After the spotify command, type the name of a song using " ", and the app will return data on the selected song. 
+* After the concert command, type the name of an artist using " ", and the app will return data on concert dates for that artist.
+* After the movie command, type the name of a movie using " ", and the app will return data on that movie.
+* The last command above uses fs to read text entered on the random.txt file and will return spotify data for "I Want it That Way".
+
+### Prerequisites
+
+If you would like to run this program on your own machine, you will need to supply your own .env file containing your spotify ID and secret key. 
+
+
+## Authors
+
+**Rebecca Shieh** 
--- liri.js
+++ liri.js
@@ -8,13 +8,39 @@ var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
 var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
 
 //Search variable to be used in each API call
 var search = process.argv[3];
-// var spotifyDefault = "The Sign";
 
+var movieURL = `http://www.omdbapi.com/?t=${search}&apikey=eb603be6`
+
+// When no song title provided, defaults to specific song, The Sign.
+function lookupSpecificSong() {
+
+	// Calls Spotify API to retrieve a specific track, The Sign, Ace of Base.
+	spotify.search({type: 'track', id: '3DYVWvPh3kGwPasp7yjahc'}, function(err, response) {
+		if (err) {
+			logOutput.error(err);
+			return
+		}
+
+		// Prints the artist, track name, preview url, and album name.
+    console.log(
+      `Preview URL: ${response.tracks.items[0].external_urls.spotify}`
+    );
+    console.log(`Artist: ${response.tracks.items[0].artists[0].name}`);
+    console.log(`Song Title: ${response.tracks.items[0].name}`);
+    console.log(`Album Name: ${response.tracks.items[0].album.name}`);
+	});
+}
 //Spotify API call
-if (argument === "spotify-this-song") {
+
+function music() {
+  if (search === ""){
+    lookupSpecificSong();
+  } else {
   spotify
     .search({ type: "track", query: search })
     .then(function(response) {
@@ -28,44 +54,132 @@ if (argument === "spotify-this-song") {
     .catch(function(err) {
       console.log(err);
     });
-  //***How to handle if no song entered?***/
-  // if (search == null){
-  //   spotify.search({ type: 'track', query: spotifyDefault});
-  // };
+  fs.appendFile("log.txt", `You entered "${argument} ${search}"\n`, function(
+    err
+  ) {
+    if (err) throw err;
+  });
+}
 }
 
+//***How to handle if no song entered?***/
+// if (search == null){
+//   spotify.search({ type: 'track', query: spotifyDefault});
+// };
+
+
 //Bands in town API
-if (argument === "concert-this"){
+function bands() {
   axios
-  .get(
-    `https://rest.bandsintown.com/artists/${search}/events?app_id=codingbootcamp`
-  )
-  .then(function(response) {
-    console.log(response.data);
-    // console.log(response.status);
-    // console.log(response.statusText);
-    // console.log(response.headers);
-    // console.log(response.config);
-  })
-  .catch(function(err) {
-    console.log(err);
-  })
-};
+    .get(
+      `https://rest.bandsintown.com/artists/${search}/events?app_id=codingbootcamp`
+    )
+    .then(function(response) {
+      var date = response.data[1].datetime;
+      moment(date).format("MM/DD/YYYY");
+      console.log(JSON.stringify(response.data, null, 2));
+      // console.log(`Artist: ${response.data[1].lineup}`);
+      // console.log(`Venue Name: ${response.data[1].venue.name}`);
+      // console.log(`Location: ${response.data[1].venue.city}, ${response.data[1].venue.country}`);
+      // console.log(`Date: ${date}`);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+  fs.appendFile("log.txt", `You entered "${argument} ${search}"\n`, function(
+    err
+  ) {
+    if (err) throw err;
+  });
+}
 
 //OMDB API
-if (argument === "movie-this"){
-axios
-  .get(
-    `http://www.omdbapi.com/?t=${search}&apikey=eb603be6`
-  )
-  .then(function(response) {
-    console.log(response.data.ratings);
-    // console.log(response.status);
-    // console.log(response.statusText);
-    // console.log(response.headers);
-    // console.log(response.config);
-  })
-  .catch(function(err) {
-    console.log(err);
-  })
+function omdb() {
+  if (search === ""){
+  axios
+    .get(`http://www.omdbapi.com/?t=Mr+Nobody&apikey=eb603be6`)
+    .then(function(response) {
+      console.log(JSON.stringify(response.data, null, 2));
+      // console.log(`Movie Title: ${response.Title}`);
+      // console.log(`Release Year: ${response.data.Year}`);
+      // console.log(`IMDB Rating: ${response.data.Ratings[0].Value}`);
+      // console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
+      // console.log(`Production Country: ${response.data.Country}`);
+      // console.log(`Movie Language: ${response.data.Language}`);
+      // console.log(`Movie Plot: ${response.data.Plot}`);
+      // console.log(`Actors: ${response.data.Actors}`);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+//   } else {
+//     axios
+//     .get(movieURL)
+//     .then(function(response) {
+//       // console.log(JSON.stringify(response.data, null, 2));
+//       console.log(`Movie Title: ${response.data.Title}`);
+//       console.log(`Release Year: ${response.data.Year}`);
+//       console.log(`IMDB Rating: ${response.data.Ratings[0].Value}`);
+//       console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
+//       console.log(`Production Country: ${response.data.Country}`);
+//       console.log(`Movie Language: ${response.data.Language}`);
+//       console.log(`Movie Plot: ${response.data.Plot}`);
+//       console.log(`Actors: ${response.data.Actors}`);
+//     })
+//     .catch(function(err) {
+//       console.log(err);
+//     });
+//   fs.appendFile("log.txt", `You entered "${argument} ${search}"\n`, function(
+//     err
+//   ) {
+//     if (err) throw err;
+//   });
+// }
+  }
+  
+
+//do-what-it-says command
+// Uses fs node package to take the text inside random.txt,
+// and do something with it.
+function doWhatItSays() {
+
+	fs.readFile("random.txt", "utf8", function(err, data) {
+		if (err) {
+			logOutput.error(err);
+		} else {
+
+			// Creates array with data.
+			var randomArray = data.split(",");
+
+			// Sets action to first item in array.
+			argument = randomArray[0];
+
+			// Sets optional third argument to second item in array.
+			search = randomArray[1];
+
+			// Calls main controller to do something based on action and argument.
+			music();
+		}
+	});
+}
+    fs.appendFile("log.txt", `You entered "${argument} ${search}"\n`, function(
+      err
+    ) {
+      if (err) throw err;
+    });
+
+
+//Command arguments
+if (argument === "movie-this") {
+  omdb();
 };
+if (argument === "concert-this") {
+  bands();
+};
+if (argument === "spotify-this-song") {
+  music();
+};
+if (argument === "do-what-it-says") {
+  doWhatItSays();
+};
+}
